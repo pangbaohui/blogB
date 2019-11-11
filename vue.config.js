071ab9a71a3e929c5431d47d280fbaaa12dc9787
pangbaohui <1043937296@qq.com>
2019-11-11 18:08:52 +0800

    做一个自己博客的后台

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,19 @@
+const path = require('path')
+const resolve = dir => path.join(__dirname, dir)
+const BASE_URL = process.env.NODE_ENV === 'procution' ? '/iview-admin' : '/' // 生产环境的地址从域名的根目录下开始指定
+
 module.exports = {
-  lintOnSave: false
+  lintOnSave: false,
+  baseUrl: BASE_URL,
+  chainWebpack: config => {
+    config.resolve.alias
+      .set('@', resolve('src'))
+      .set('_c', resolve('src/components'))
+  },
+  // 打包时不生成.map文件
+  productionSourceMap: false,
+  // 没有匹配到静态文件的请求都代理到配置的地址
+  devServer: {
+    proxy: 'http://localhost:4000'
+  }
 }
